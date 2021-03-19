63c83712a6951547e42360fb5b198aff2c85f1cf
Zhao KK <kikakkz@hotmail.com>
2021-03-19 22:04:22 +0800

    Use right etcd host

--- types/consts.go
+++ types/consts.go
@@ -4,5 +4,5 @@ const (
 	ExchangeKeyAPI = "/api/v0/client/exchange_key"
 	LoginAPI       = "/api/v0/client/login"
 	HeartbeatAPI   = "/api/v0/client/heartbeat"
-	EtcdHost       = "http://etcd.npool.top"
+	EtcdHost       = "etcd.npool.top:2379"
 )
